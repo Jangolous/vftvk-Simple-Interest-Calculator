a72d0cffd6c06adcd215fd637dd21eabe5bb560a
Jangolous <99839014+Jangolous@users.noreply.github.com>
2022-02-16 22:33:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+  let  p = document.getElementById("principal").value;
+   let r = document.getElementById("Rate").value;
+    let y = document.getElementById("years").value;
+		 let rate = "Rate"/100
+      let I = p*rate*y
 }
-        
