7a0150cc398a59a19cfc2b590cab7e8ef5b38317
Jangolous <99839014+Jangolous@users.noreply.github.com>
2022-02-19 22:03:23 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 function compute()
 {
-  let  p = document.getElementById("principal").value;
-   let r = document.getElementById("Rate").value;
-    let y = document.getElementById("years").value;
-		 let rate = "Rate"/100
-      let I = p*rate*y
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
+
+   //Check if username field is empty
+      if(amount.value >= "0"){
+            alert("Enter a positive number");
+            amount.focus();
+     return false;
+    }
+    
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+   return true;   
+ }     
