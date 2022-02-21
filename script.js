a3faa868834868cb47f46909b51717c0c38f904e
Jangolous <99839014+Jangolous@users.noreply.github.com>
2022-02-20 20:25:58 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
-function compute()
-{
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);
-
-    function updateRate() 
+//This is the function to show the selected rate on the screen
+function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
+//This is the function to compute the interest with the input
+function compute() {
+  var principal = document.getElementById("principal").value;
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+  var interest = principal * years * rate / 100;
+  var year = new Date().getFullYear() + parseInt(years);
 
-   //Check if username field is empty
-      if(amount.value >= "0"){
-            alert("Enter a positive number");
-            amount.focus();
-     return false;
-    }
-    
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
-   return true;   
- }     
+  document.getElementById("result").innerHTML = "If you deposit " + principal + ",\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + interest + ",\<br\>in the year " + year + "\<br\>"
+}
+//This function checks if there is a valid input for the principal variable
+function checkData() {
+if (principal.value >= 0) {
+    alert("Enter a positive number");
+    principal.focus();
+    return false;
+  }
+if (principal.value == "") {
+    alert("Enter a positive number");
+    principal.focus();
+    return false;
+  }
+  
+  return true;
+  }
