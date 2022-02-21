2c15c4e6d3eafc57945395f350c55090cab0b4fa
Jangolous <99839014+Jangolous@users.noreply.github.com>
2022-02-20 22:46:16 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+//This function checks if there is a valid input for the principal variable
+function validate(){
+    let amount = document.getElementById("principal");
+    
+    if(amount.value >= 0){
+          alert("Enter a positive number");
+          amount.focus();
+          return false;
+    }
+    if(amount.value == ""){
+        alert("Enter a positive number");s
+        amount.focus();
+        return false;
+  }
+   alert("Form validation is successful.")
+    return true;
+  }
+//We declare variables
+  var principal = document.getElementById("principal").value;
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+  var interest = principal * years * rate / 100;
+  var year = new Date().getFullYear() + parseInt(years);
+
 //This is the function to show the selected rate on the screen
 function updateRate() 
 {
@@ -6,26 +30,6 @@ function updateRate()
 }
 //This is the function to compute the interest with the input
 function compute() {
-  var principal = document.getElementById("principal").value;
-  var rate = document.getElementById("rate").value;
-  var years = document.getElementById("years").value;
-  var interest = principal * years * rate / 100;
-  var year = new Date().getFullYear() + parseInt(years);
-
   document.getElementById("result").innerHTML = "If you deposit " + principal + ",\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + interest + ",\<br\>in the year " + year + "\<br\>"
 }
-//This function checks if there is a valid input for the principal variable
-function checkData() {
-if (principal.value >= 0) {
-    alert("Enter a positive number");
-    principal.focus();
-    return false;
-  }
-if (principal.value == "") {
-    alert("Enter a positive number");
-    principal.focus();
-    return false;
-  }
-  
-  return true;
-  }
+
