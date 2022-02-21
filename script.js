3eac7a58cec3c9c78b9aa51d31537fc6fe6b8593
Jangolous <99839014+Jangolous@users.noreply.github.com>
2022-02-20 23:26:18 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 //This function checks if there is a valid input for the principal variable
 function validate(){
-    let amount = document.getElementById("principal");
+    var amount = document.getElementById("principal");
     
-    if(amount.value >= 0){
+    if(amount.value <= 1){
           alert("Enter a positive number");
           amount.focus();
           return false;
-    }
-    if(amount.value == ""){
-        alert("Enter a positive number");
-        amount.focus();
-        return false;
+    } else {
+    //This is the function to compute the interest with the input
+function compute() {
+            document.getElementById("result").innerHTML = "If you deposit " + principal + ",\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + interest + ",\<br\>in the year " + year + "\<br\>"
+          }    
   }
-   alert("Form validation is successful.")
-    return true;
-  }
+  
 //We declare variables
   var principal = document.getElementById("principal").value;
   var rate = document.getElementById("rate").value;
@@ -28,8 +26,3 @@ function updateRate()
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
-//This is the function to compute the interest with the input
-function compute() {
-  document.getElementById("result").innerHTML = "If you deposit " + principal + ",\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + interest + ",\<br\>in the year " + year + "\<br\>"
-}
-
